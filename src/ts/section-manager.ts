5dd46837ccc595bd92da4e7cf0d44c9538dcf688
Damian Kolczyński <damian-kolczynski@o2.pl>
2021-10-23 17:01:33 +0200

    Custom shortcut for switching sections

--- src/ts/section-manager.ts
+++ src/ts/section-manager.ts
@@ -1,13 +1,27 @@
 import { clss } from './utils';
 
 export class SectionManager {
-  constructor() {}
+  private gradingShown = false;
+
+  constructor() {
+    this.assignListeners();
+  }
 
   public showGrading() {
+    this.gradingShown = true;
     clss('grade-section')[0].setAttribute('name', 'active');
   }
 
   public hideGrading() {
+    this.gradingShown = false;
     clss('grade-section')[0].removeAttribute('name');
+  }
+
+  private assignListeners() {
+    window.addEventListener('keydown', (ev) => {
+      if (ev.code === 'KeyS') {
+        this.gradingShown ? this.hideGrading() : this.showGrading();
+      }
+    });
   }
 }
