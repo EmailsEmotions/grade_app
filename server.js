3e26384796028ef9590185931213d742891eb571
damian <damian-kolczynski@o2.pl>
2021-10-26 13:20:50 +0200

    Deploy server update

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  let requestedFile = `${__dirname}/dist${req.url}`;
-
-  if (req.url === '/') {
-    requestedFile = `${__dirname}/dist/index.html`;
-  }
-
-  fs.readFile(requestedFile, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-});
-
-server.listen(port);
-console.log(`Server is listening on port ${port}`);
+const express = require('express');
+const path = require('path');
+const app = express();
+ 
+app.use('/', express.static(path.join(__dirname, 'dist')));
+app.listen(80);
