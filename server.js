a0819de4fd5e77306c23452efe8b044db7f13041
damian <damian-kolczynski@o2.pl>
2021-10-26 13:34:22 +0200

    More server stuff

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
- 
-app.use('/', express.static(path.join(__dirname, 'dist')));
-app.listen(80);
+// Create an express app
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 8080;
+
+// Use the static
+app.use(express.static("dist"));
+
+// Start the server
+app.listen(port, 
+	() => console.log(`Server is running on port ${port}...`));
